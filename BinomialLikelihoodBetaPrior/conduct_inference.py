62fc0a506b94b51c68044d88dd79c069f9d80ede
Christian <christian.wachauf85@gmail.com>
2020-07-08 01:47:28 +0200

    added plotting of the samples and their distribution

--- BinomialLikelihoodBetaPrior/conduct_inference.py
+++ BinomialLikelihoodBetaPrior/conduct_inference.py
@@ -1,35 +1,29 @@
 import pandas as pd
 import numpy as np
 import pystan
+import matplotlib.pyplot as plt
+from scipy.stats import beta
+import seaborn as sns
 import os
 
 PATH_STAN_MODELS = 'models'
-FN_LR_MODEL = 'logistic_regression.stan'
-PATH_STAN_OUTPUT = 'output'
-FN_STAN_OUTPUT = 'samples_logistic_regression_300_data_points.csv'
+FN_BLBP_MODEL = 'bin_lik_beta_prior.stan'
+PATH_OUTPUT_FILES = 'output'
+
+FN_STAN_OUTPUT = 'samples_bin_lik_beta_prior.csv'
+
+FN_OUTPUT_FIGURE = "stan_samples_binom_lik_beta_prior.png"
 
 # parameters for simulating data from a logistic regression model:
-NUM_DATA_POINTS = 300
-LOG_REG_A = 0.6
-LOG_REG_B = -4.0
-HOURS_STUDIED_MIN = 1.0
-HOURS_STUDIED_MAX = 12.0
 NUM_SAMPLES = 10000
 
-def logistic_function(x,a,b):
-    return 1.0/(1.0+np.exp(-(a*x+b)))
-
 if __name__=='__main__':
-    np.random.seed(1)
-    hours = np.random.uniform(low=HOURS_STUDIED_MIN,high=HOURS_STUDIED_MAX,size=NUM_DATA_POINTS)
-    probs = [logistic_function(hour,a=LOG_REG_A,b=LOG_REG_B) for hour in hours]
-    passed = [np.random.binomial(n=1,p=p_i,size=1)[0] for p_i in probs]
+    data_for_stan = {'N_heads':3,
+                     'N_tails':0}
 
-    sim_data_dict = {"hours":hours,"probabilities":probs,"passed":passed}
-    data_for_stan = {"N":NUM_DATA_POINTS,"x":hours,"y":passed}
     print(data_for_stan)
-    # Compile the model
-    comp_path_stan_model = os.path.join(PATH_STAN_MODELS,FN_LR_MODEL)
+
+    comp_path_stan_model = os.path.join(PATH_STAN_MODELS,FN_BLBP_MODEL)
 
     with open(comp_path_stan_model,'rt') as sm_file:
         model_code_str = sm_file.read()
@@ -40,8 +34,25 @@ if __name__=='__main__':
                              iter=2*NUM_SAMPLES,
                              chains=1
                             )
-    print(stan_fit)
-    df_stan_samples = pd.DataFrame({"alpha":list(stan_fit["alpha"]),"beta":list(stan_fit["beta"])})
 
-    comp_path_samples = os.path.join(PATH_STAN_OUTPUT,FN_STAN_OUTPUT)
-    df_stan_samples.to_csv(comp_path_samples)
+    print(stan_fit)
+    thetas = np.array(stan_fit["theta"])
+    comp_path_figure = os.path.join(PATH_OUTPUT_FILES,FN_OUTPUT_FIGURE)
+    sns.set(style='whitegrid', rc={"grid.linewidth": 0.2})
+    sns.set_context("paper", font_scale=2.0)
+    fig, ax = plt.subplots(nrows=1, ncols=2,figsize=(14,6))
+
+    sns.scatterplot(range(len(thetas)),thetas,ax=ax[0],s=30)
+    ax[0].set_title("Samples for theta")
+    ax[0].set_xlabel("sample number")
+    ax[0].set_ylabel("theta")
+
+    sns.distplot(thetas,ax=ax[1],kde=False,hist=True,norm_hist=True)
+    xs=np.linspace(0.0,1.0,101)
+    sns.lineplot(xs,beta.pdf(xs,a=5,b=2),color="black")
+    ax[1].set_title("Distribution of samples")
+    ax[1].set_xlabel("theta")
+    ax[1].set_ylabel("probability density")
+
+    fig.savefig(comp_path_figure)
+    plt.close(fig)
